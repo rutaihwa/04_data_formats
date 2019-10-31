700f5d5b7cbfad364f5b65ed05a77170eb1fb2ea
Rutaihwa <rutaihwa@gmail.com>
2019-10-31 18:31:33 +0100

    Some ideas for serilaization and deserialization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate futures;
+extern crate hyper;
+extern crate rand;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+
 fn main() {
     println!("Hello, world!");
+}
+
+// RngResponse
+#[derive(Serialize)]
+struct RngResponse {
+    value: f64,
+}
+
+// RngRequest
+#[derive(Deserialize)]
+#[serde(tag = "distribution", content = "parameters", rename_all = "lowercase")]
+enum RngRequest {
+    Uniform { range: Range<i32> },
+    Normal { mean: f64, std_dev: f64 },
+    Bernoulli { p: f64 },
 }
